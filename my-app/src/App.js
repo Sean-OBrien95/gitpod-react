5f7a1943a1d76704d08154de49dffaf4050e5b93
Seán O'Brien <sean95ob@gmail.com>
2024-03-06 18:49:39 +0000

    complete searchbar challenege

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,10 +16,12 @@ import LifeCyclesCDM from './components/LifeCyclesCDM';
 import LifeCyclesCDU from './components/LifeCyclesCDU';
 import LifeCyclesCWU from './components/LifeCyclesCWU';
 import ControlledForm from './components/ControlledForm';
+import UncontrolledForm from './components/UncontrolledForm';
+import SearchBar from './components/SearchBar'
 
 function App() {
   return <div className="App">
-      <ControlledForm />
+      <SearchBar />
   </div>;
 };
 
