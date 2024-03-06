997c525778f96720d86f9ef4826b045d3033ab3d
Seán O'Brien <sean95ob@gmail.com>
2024-03-06 14:54:41 +0000

    Updated ComponentDidMount class

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,10 +12,11 @@ import ConditionalRenderingFunctional from './components/ConditionalRenderingFun
 import NestingComponents from './components/NestingComponents';
 import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
 import RenderingLists from './components/RenderingLists';
+import LifeCyclesCDM from './components/LifeCyclesCDM';
 
 function App() {
   return <div className="App">
-      <RenderingLists />
+      <LifeCyclesCDM />
   </div>;
 };
 
---
+++ my-app/src/components/LifeCyclesCDM.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import LifeCyclesCDMChild from './LifeCyclesCDMChild'
+
+export class LifeCyclesCDM extends Component {
+    constructor(props) {
+        console.log('Constructor called')
+      super(props)
+    
+      this.state = {
+        data: 'Loading...'
+      }
+    }
+    getData() {
+        console.log('getData() called')
+        setTimeout(() => {
+            console.log('Data fetched')
+            this.setState({
+                data: 'Loaded'
+            })
+        }, 3000)
+    }
+    componentDidMount() {
+        console.log('componenet mounted')
+        this.getData()
+    }
+    render() {
+        console.log('render method called')
+        return (
+            <div>
+                <h1>{this.state.data}</h1>
+                <LifeCyclesCDMChild />
+            </div>
+        )
+    }
+}
+
+export default LifeCyclesCDM
