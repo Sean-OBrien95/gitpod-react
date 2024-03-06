ec64eb1afcb6ff451adf1af05efe4cd1ac15c611
Seán O'Brien <sean95ob@gmail.com>
2024-03-06 14:26:08 +0000

    Add RederingList and book.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,10 +11,11 @@ import ConditionalRenderingClass from './components/ConditionalRenderingClass';
 import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
 import NestingComponents from './components/NestingComponents';
 import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
+import RenderingLists from './components/RenderingLists';
 
 function App() {
   return <div className="App">
-      <MethodsAsPropsParent />
+      <RenderingLists />
   </div>;
 };
 
